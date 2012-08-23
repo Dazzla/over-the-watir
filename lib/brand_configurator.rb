bee07a9699d08d1c7869798c9ad73477dd3442b4
Dazzla <dazzlebown@gmail.com>
2012-08-23 20:44:20 +0100

    Locator config moved to site_config.yml

--- lib/brand_configurator.rb
+++ lib/brand_configurator.rb
@@ -1,44 +1,28 @@
 class BrandConfigurator
   #Will fetch the element selectors from yaml config files
-
+  require 'yaml'
 
   attr_reader :route, :date, :itinerary_type, :no_of_pax,
               :no_results_error_container_element, :submit_button_element
+
 
   def initialize(brand)
     #Used to select correct element configurations to give brand flexibility
     #Will eventually use a config file - hard-coded stub for now.
     #brand is specified by the customer in the features file
+    config_file = File.join(File.dirname(__FILE__), '..', 'config', 'site_config.yml')
 
-    @brand = brand
-    @route  =             {
-                            :attribute => :id,
-                            :origin  => "uw_flight_origin_input",
-                            :destination =>  "uw_flight_destination_input"
-                          }
+    config = YAML.load_file config_file
 
-    @date =               {
-                            :attribute => :id,
-                            :outgoing_date => "uw_flight_dep_date_input",
-                            :incoming_date => "uw_flight_return_date_input"
-                          }
 
-    @itinerary_type =     {
-                            :attribute => :id,
-                            :single =>  "uw_flight_type0",
-                            :return =>  "uw_flight_type1"
-                          }
+    @brand          = brand
+    @route          = config[:route]
+    @date           = config[:date]
+    @itinerary_type = config[:itinerary_type]
+    @no_of_pax      = config[:no_of_pax]
 
-    @no_of_pax =          {
-                            :attribute => :id,
-                            :adults =>    "uw_flight_adults_input",
-                            :children =>  "uw_flight_children_input",
-                            :seniors  =>  "uw_flight_seniors_input",
-                            :infants =>   "" #Not required here
-                          }
-
-    @no_results_error_container_element = "divFlightResultErrTitle"
-    @submit_button_element = "uw_flight_submit_lnk"
+    @no_results_error_container_element = config[:user_messages][:no_results_error_container_element]
+    @submit_button_element              = config[:generic_page_elements][:submit_button_element]
   end
 
 end
